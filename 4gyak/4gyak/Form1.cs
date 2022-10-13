66fe20b9b6070439e821dcb23197f38396ab18c0
VargaFerencINFO <ferenc.varga@stud.uni-corvinus.hu>
2022-10-13 20:46:21 +0200

    negyedik

--- 4gyak/4gyak/Form1.cs
+++ 4gyak/4gyak/Form1.cs
@@ -20,20 +20,25 @@ namespace _4gyak
         Excel.Application xlApp; // A Microsoft Excel alkalmazás
         Excel.Workbook xlWB; // A létrehozott munkafüzet
         Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+        private int _million = (int)Math.Pow(10, 6);
 
-       
         public Form1()
         {
             InitializeComponent();
             LoadData();
             CreateExcel();
             CreateTable();
-            
+
+        }
+        private void LoadData()
+        {
+            Flat = context.Flat.ToList();
         }
 
         private void CreateTable()
         {
-            string[] headers = new string[] {
+            string[] headers = new string[]
+            {
              "Kód",
              "Eladó",
              "Oldal",
@@ -43,6 +48,39 @@ namespace _4gyak
              "Alapterület (m2)",
              "Ár (mFt)",
              "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+            object[,] values = new object[Flat.Count, headers.Length];
+            int counter = 0;
+            foreach (var f in Flat)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+
+                if (f.Elevator == true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = String.Format("={0} / {1} * {2}",
+                    "H" + (counter + 2).ToString(),
+                    "G" + (counter + 2).ToString(),
+                    _million.ToString());
+                counter++;
+            }
+            var range = xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1)));
+            range.Value2 = values;
         }
 
         private void CreateExcel()
@@ -76,13 +114,26 @@ namespace _4gyak
                 xlWB = null;
                 xlApp = null;
             }
+           
         }
+     
 
-        private void LoadData()
+        private string GetCell(int x, int y)
         {
-            Flat = context.Flat.ToList();
-        }
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
 
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
