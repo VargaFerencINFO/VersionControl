f603452292f01f22bcdb373623b7b3aa3fc4dc02
VargaFerencINFO <ferenc.varga@stud.uni-corvinus.hu>
2022-11-03 15:37:19 +0100

    hetedik

---
+++ Fejlesztesi_Mintak/Fejlesztesi_Mintak/Entities/Car.cs
@@ -0,0 +1,22 @@
+﻿using Fejlesztesi_Mintak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fejlesztesi_Mintak.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+
+       
+    }
+}
