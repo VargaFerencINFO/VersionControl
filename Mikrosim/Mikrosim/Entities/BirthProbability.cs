1ef6f2fffa2d91a9b6bd7da77d3b1234672c97e5
VargaFerencINFO <ferenc.varga@stud.uni-corvinus.hu>
2022-12-02 12:14:30 +0100

    elso

---
+++ Mikrosim/Mikrosim/Entities/BirthProbability.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikrosim.Entities
+{
+    internal class BirthProbability
+    {
+        public int Age { get; set; }
+        public int Children_nmb { get; set; }
+    }
+}
