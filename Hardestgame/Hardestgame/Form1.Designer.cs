6fa8e303c3d5f872d2892902ba38646e6373eafd
VargaFerencINFO <ferenc.varga@stud.uni-corvinus.hu>
2022-12-05 20:04:36 +0100

    negyedik

--- Hardestgame/Hardestgame/Form1.Designer.cs
+++ Hardestgame/Hardestgame/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 18);
+            this.label1.Location = new System.Drawing.Point(12, 278);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
